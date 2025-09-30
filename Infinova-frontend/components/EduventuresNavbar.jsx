0d7f0c59103f5cf147aadf27f54872997bc770a3
Shouryan Bharote <shouryanbharote888@gmail.com>
2025-09-30 10:54:22 +0530

    handpicked all the proper code from all places

--- Infinova-frontend/components/EduventuresNavbar.jsx
+++ Infinova-frontend/components/EduventuresNavbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 
 const EduventuresNavbar = () => {
   return (
-    <nav className="eduventures-navbar bg-black h-10 md:h-10">
+    <nav className="eduventures-navbar bg-black h-[5.5 vh] md:h-[5.5 vh]">
       <div className="eduventures-navbar-inner">
         <NavLink
           to="/eduventures/forstudents"
